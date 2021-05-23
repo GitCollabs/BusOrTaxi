81f0050e7446b95d78d4c0a59971c0aff9259829
domfarr98 <domfarr98@gmail.com>
2021-05-23 17:13:35 +0100

    Refactored layout of initMap to be a little more readable

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,39 @@
 let map;
 
 function initMap() {
+
     const myLatlng = { lat: 53.4808, lng: -2.2426 };
+
     var options = {
         zoom: 14,
         center: myLatlng
     }
+
     map = new google.maps.Map(document.getElementById("map"), options);
 
     var starter;
 
-    let infoWindow = new google.maps.InfoWindow({
+    let infoWindowOne = new google.maps.InfoWindow({
         content: "Click the map to get Lat/Lng!",
         position: myLatlng,
-      });
-      infoWindow.open(map);
-      // Configure the click listener.
-      map.addListener("click", (mapsMouseEvent) => {
+    });
+
+    infoWindowOne.open(map);
+
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
         // Close the current InfoWindow.
-        infoWindow.close();
+        infoWindowOne.close();
         // Create a new InfoWindow.
-        infoWindow = new google.maps.InfoWindow({
-          position: mapsMouseEvent.latLng,
+        infoWindowOne = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng
         });
-        infoWindow.setContent(
-          JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        infoWindowOne.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
         );
         starter = mapsMouseEvent.latLng.toJSON()
         console.log(starter.lat, starter.lng);
-        infoWindow.open(map);
-      });
-
+        infoWindowOne.open(map);
+        i++;
+    });
 }
